5cae7e2a0b7ff5405b2f27c0cacb9365da2a256c
aladargento <aladargento@hotmail.com>
2025-01-25 17:00:06 +0100

    sql

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON="C:\Users\Megaport\AppData\Local\Programs\Python\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--xformers --listen --medvram --precision full --no-half --opt-split-attention --skip-torch-cuda-test --disable-nan-check
 Git pull
 call webui.bat
